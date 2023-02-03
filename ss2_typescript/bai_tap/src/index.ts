31977c9920b207ad3a847479602ac047c48b9b4e
unknown <you@example.com>
2023-02-03 13:27:31 +0700

    nb

--- ss2_typescript/bai_tap/src/index.ts
+++ ss2_typescript/bai_tap/src/index.ts
@@ -9,12 +9,14 @@ let checkFibonacci = function (num: number): number {
         return 1;
     }
     return checkFibonacci(num - 1) + checkFibonacci(num - 2);
-
-
 }
+
 let sumFibonacci: number = 0;
+
 let amountNumber: number = 6;
+
 let arr: Array<number> = [];
+
 for (let i = 0; i < amountNumber; i++) {
     arr.push(checkFibonacci(i));
     sumFibonacci += checkFibonacci(i);
