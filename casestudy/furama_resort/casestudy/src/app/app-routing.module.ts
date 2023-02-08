06aefe988d3789a828df31f1a3cab8455e5ea21d
unknown <you@example.com>
2023-02-08 17:32:42 +0700

    nb

--- casestudy/furama_resort/casestudy/src/app/app-routing.module.ts
+++ casestudy/furama_resort/casestudy/src/app/app-routing.module.ts
@@ -2,12 +2,20 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {ListFacilityComponent} from "./facilities/list-facility/list-facility.component";
 import {HomeComponent} from "./home/home.component";
+import {EditFacilityComponent} from "./facilities/edit-facility/edit-facility.component";
+import {CreateFacilityComponent} from "./facilities/create-facility/create-facility.component";
+import {ListCustomerComponent} from "./customer/list-customer/list-customer.component";
 
 
 const routes: Routes = [
 
   {path:"home",component:HomeComponent},
-  {path:"facilities",component:ListFacilityComponent}
+  {path:"facilities",component:ListFacilityComponent},
+  {path:"facilities/edit/:id", component: EditFacilityComponent},
+  {path:"facilities/create", component:CreateFacilityComponent},
+  {path:"customers",component:ListCustomerComponent}
+
+
 
 
 
--- casestudy/furama_resort/casestudy/src/app/customer/list-customer/list-customer.component.ts
+++ casestudy/furama_resort/casestudy/src/app/customer/list-customer/list-customer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Customer} from "../../model/customer";
 
 @Component({
   selector: 'app-list-customer',
@@ -6,7 +7,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list-customer.component.css']
 })
 export class ListCustomerComponent implements OnInit {
+  customers:Customer[]=[
 
+    {
+      id: 1,
+      name: 'Duc Vinh',
+      dateOfBirth: '22/11/2000',
+      gender: 1,
+      idCard: '12345',
+      phoneNumber: '123444444',
+      email: 'vinh@gmail.com',
+      customerType: 'Diamond',
+      address: 'Quang Nam'
+    },
+    {
+      id: 2,
+      name: 'Pham Thi Vi',
+      dateOfBirth: '02/10/1998',
+      gender: 0,
+      idCard: '12344454',
+      phoneNumber: '028237373',
+      email: 'vithipham@gmail.com',
+      customerType: 'Diamond',
+      address: 'Quang Nam'
+    }
+  ];
   constructor() { }
 
   ngOnInit(): void {
