c421dd48d470518cddcd3e756ec13c6d07864f2e
unknown <you@example.com>
2023-02-09 10:19:30 +0700

    nb

--- ss5_component_interaction/exercise/angular-countdown-timer-app/src/app/countdown-timer/countdown-timer.component.ts
+++ ss5_component_interaction/exercise/angular-countdown-timer-app/src/app/countdown-timer/countdown-timer.component.ts
@@ -1,12 +1,12 @@
 
-import {Component, EventEmitter, Input, OnChanges, OnDestroy, OnInit, Output, SimpleChanges} from '@angular/core';
+import {Component, EventEmitter, Input, OnDestroy, OnInit, Output} from '@angular/core';
 
 @Component({
   selector: 'app-countdown-timer',
   templateUrl: './countdown-timer.component.html',
   styleUrls: ['./countdown-timer.component.css']
 })
-export class CountdownTimerComponent implements OnInit, OnChanges, OnDestroy {
+export class CountdownTimerComponent implements OnInit, OnDestroy {
   message = '';
   remainingTime: number;
   @Input()
@@ -15,14 +15,7 @@ export class CountdownTimerComponent implements OnInit, OnChanges, OnDestroy {
   finish = new EventEmitter<boolean>();
   private intervalId = 0;
 
-  ngOnChanges(changes: SimpleChanges) {
-    if ('seconds' in changes) {
-      let v = changes.seconds.currentValue;
-      v = typeof v === 'undefined' ? 11 : v;
-      const vFixed = Number(v);
-      this.seconds = Number.isNaN(vFixed) ? 11 : vFixed;
-    }
-  }
+
 
   clearTimer() {
     clearInterval(this.intervalId);
